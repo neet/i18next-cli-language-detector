1832b2f2f7f8659ae8c27114d2a812e2a3b28158
Neetshin <n33t5hin@gmail.com>
2019-12-18 01:16:09 +0900

    Add an example

---
+++ examples/greeting.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import i18next from 'i18next';
+import I18nextCLILanguageDetector from '../dist';
+
+i18next.use(I18nextCLILanguageDetector).init({
+  resources: {
+    ja: { translations: { greeting: '🇯🇵こんにちは' } },
+    en: { translations: { greeting: '🇬🇧Hello' } },
+    zh: { translations: { greeting: '🇨🇳你好' } },
+    fr: { translations: { greeting: '🇫🇷Bonjour' } },
+  },
+});
+
+console.log(i18next.t('translations:greeting'));
